cb301974ceaa9b482d80050884039e1377af8722
tucond <kikkikukkuku@gmail.com>
2022-11-04 00:54:46 +0900

    Update config for using Render.com

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,6 @@
 set -o errexit
 
 bundle install
-#bundle exec rake assets:precompile
-#bundle exec rake assets:clean
-bundle exec rake db:create RAILS_ENV=production
-bundle exec rake db:migrate RAILS_ENV=production
+
+# bundle exec rake db:create RAILS_ENV=production
+# bundle exec rake db:migrate RAILS_ENV=production
