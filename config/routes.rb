dd705b5c6c8577b66f68809a0a7efac081e380b6
tucond <kikkikukkuku@gmail.com>
2022-11-04 09:41:45 +0900

    Delete unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'youtube_api/new'
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
