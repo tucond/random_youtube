8838f19f1cd3549a1ac722e07e2a704bd702ce19
tucond <kikkikukkuku@gmail.com>
2022-11-09 00:17:19 +0900

    Fixed CORS error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '127.0.0.1:3000', 'localhost:3000', 'https://random-youtube-frontend2.vercel.app'
+    origins '*'
 
     resource "*",
       headers: :any,
