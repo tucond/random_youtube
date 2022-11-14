fb2a367d2eeff828564cf3d4c477c9ee6e48a4ea
tucond <kikkikukkuku@gmail.com>
2022-11-15 08:06:45 +0900

    Update render.com configuration file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 bundle install
 
+bundle exec rails db:create RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
+
 # bundle exec rake db:create RAILS_ENV=production
 # bundle exec rake db:migrate RAILS_ENV=production
